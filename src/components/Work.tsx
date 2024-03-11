29d80938947f119236b15806d7f7852bfe26b6f6
RishithaVanamala <rishithavanamala@gmail.com>
2024-03-10 22:57:15 -0400

    Updated work experience and resume

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -15,42 +15,7 @@ const Work = () => {
       </main>
 
       <section>
-        <Article>
-          <div className="relative pb-12 md:col-span-2">
-            <div className="sticky top-28">
-              <svg
-                className="absolute left-[-38px] md:left-[-50px] color-bg-default color-fg-muted bg-white dark:bg-dark rounded-full dark:text-gray-500"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="28"
-                height="28"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M15.5 11.75a3.5 3.5 0 11-7 0 3.5 3.5 0 017 0zm1.444-.75a5.001 5.001 0 00-9.888 0H2.75a.75.75 0 100 1.5h4.306a5.001 5.001 0 009.888 0h4.306a.75.75 0 100-1.5h-4.306z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-              <a>
-                <h3 className="p-0 m-0 pt-2 text-4xl font-bold mb-4 dark:text-gray-200">
-                  Teaching Assistant
-                </h3>
-              </a>
-              <p className="p-0 m-0 text-3xl mb-2 dark:text-gray-200">
-                Syracuse University
-              </p>
-              <p className="p-0 m-0 text-2xl dark:text-gray-200 text-gray-500 before:content-['['] dark:before:text-teal-500 before:text-indigo-500 after:text-indigo-500 after:font-bold before:font-bold after:content-[']'] dark:after:text-teal-500 after:text-indigo-500after:font-bold">
-                &nbsp; Aug 2023 - Present &nbsp;
-              </p>
-            </div>
-          </div>
-          <p className="relative pb-4 md:border-b dark:text-gray-200 md:border-gray-300 md:dark:border-gray-400 md:col-span-3 text-[1.7rem] font-sans">
-          • Teaching Assistant in Data Structures and Algorithms: Mentored 120 students in mastering coding techniques, boosting
-their algorithmic proficiency.
-            <br />
-           • Conducted engaging lab sessions, boosting academic success with personalized support and office hours. <br />
-          </p>
-        </Article>
+       
         
       </section>
       <section>
@@ -70,7 +35,17 @@ their algorithmic proficiency.
                   fill="currentColor"
                 ></path>
               </svg>
-              <a>
+              <a
+                href="https://github.com/Bhanu1776/IT-Dept-Website"
+                target="_blank"
+                rel="noreferrer"
+                onClick={() => {
+                  ReactGa.event({
+                    category: "Internships",
+                    action: "Checking Internship repository",
+                  });
+                }}
+              >
                 <h3 className="p-0 m-0 pt-2 text-4xl font-bold mb-4 dark:text-gray-200">
                   Software Engineer
                 </h3>
@@ -84,29 +59,16 @@ their algorithmic proficiency.
             </div>
           </div>
           <p className="relative pb-4 md:border-b dark:text-gray-200 md:border-gray-300 md:dark:border-gray-400 md:col-span-3 text-[1.7rem] font-sans">
-          <span className="text-3xl font-bold dark:text-gray-200 ">Django Developer:</span><br/>
-          • Designed and developed over 10 new high-performance, and efficient Django based REST API solutions, while
-            concurrently improving existing ones for optimal performance. <br />
-          • Created and maintained technical API documentation for new and existing APIs using Swagger. <br />
+          • Designed and developed over 10 new high-performance and efficient Django REST API solutions, concurrently improving
+            existing ones for optimal performance. Documented and updated technical API documentation.<br />
+          • Headed API integration for 3 clients, collaborating with cross-functional teams to ensure seamless functionality.<br />
+          • Deployed end-to-end cloud-based solutions utilizing AWS services such as DynamoDB, EMR, EC2, and Redshift to enhance
+            infrastructure scalability and reliability.<br />
+          • Handled production incidents by monitoring Sentry logs, ensuring 99% system uptime during migrations and updates.<br />
+          • Resolved technical issues, implemented unit tests in existing applications using test-driven development (TDD)
+            methodologies and Postman, reducing overall bugs by 10%.<br />
+          • Implemented payment solutions with secure card linking and increased efficiency by 40% with automated Celery jobs.<br />
           • Conducted code reviews to enforce high code quality standards, leading to a 50% reduction in post-deployment bugs.<br />
-          • Addressed and handled critical production incidents, ensuring 99.9% system uptime during migrations and updates.<br />
-          • Resolved technical issues and implemented unit tests in existing applications, reducing bugs by 30%.<br />
-          • Served as technical point of contact for customer integrations, collaborating with cross-functional teams and clients to
-            ensure seamless integrations through API solutions.<br />
-          • Increased system efficiency by 40% with automation scripts and celery jobs.<br />
-          • Led card linking integration projects, and fortified data security with email and mobile verification checks.<br />
-          
-          <br />
-          
-          <span className="text-3xl font-bold dark:text-gray-200">Java Developer:</span><br/>
-          • Developed Spring Boot REST API microservices, optimizing CRUD operations and achieving a 20% increase in API
-            throughput. <br />
-          • Utilized key AWS services, such as DynamoDB, EMR, EC2, and Lambda, to construct cloud-based solutions.<br />
-          • Architected a flexible framework for webhooks, which enabled automatic email triggers and data synchronization,
-            enhancing end-user experience as evidenced by a 15% increase in client engagement metrics such as analytical dashboards.<br />
-          • Played a pivotal role in Kafka integration, facilitating efficient data transfer across multiple organizations, which improved
-            API performance by about 20% and upgraded data handling capabilities.<br />
-                              
           </p>
         </Article>
       </section>
@@ -137,17 +99,56 @@ their algorithmic proficiency.
                 Loyalty Juggernaut
               </p>
               <p className="p-0 m-0 text-2xl dark:text-gray-200 text-gray-500 before:content-['['] dark:before:text-teal-500 before:text-indigo-500 after:text-indigo-500 after:font-bold before:font-bold after:content-[']'] dark:after:text-teal-500 after:text-indigo-500after:font-bold">
-                &nbsp; Aug 2023 - Present &nbsp;
+                &nbsp; Jan 2021 - Jun 2021 &nbsp;
               </p>
             </div>
           </div>
           <p className="relative pb-4 md:border-b dark:text-gray-200 md:border-gray-300 md:dark:border-gray-400 md:col-span-3 text-[1.7rem] font-sans">
           • Upgraded existing APIs with advanced data filters and expanded response fields, resulting in 20% improved functionality.<br />
           • Integrated Git for version control, facilitating collaboration. Orchestrated CI/CD pipelines for automated software delivery,
-            boosting deployment efficiency. Deployed Datadog for API performance monitoring.<br />
+            boosting deployment efficiency, utilizing Jenkins and Docker. Deployed Datadog for API performance optimization.<br />
           • Developed database-driven applications working with relational databases and non-relational databases.<br />
-          • Adapted Agile methodology for development, orchestrating daily Scrum meetings to drive collaborative projects through
-            the software development life cycle (SDLC).<br />
+          • Adapted Agile methodology for development, orchestrating daily Scrum meetings to drive collaborative projects through the
+            software development life cycle (SDLC).<br />
+          </p>
+        </Article>
+
+        <Article>
+          <div className="relative pb-12 md:col-span-2">
+            <div className="sticky top-28">
+              <svg
+                className="absolute left-[-38px] md:left-[-50px] color-bg-default color-fg-muted bg-white dark:bg-dark rounded-full dark:text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="28"
+                height="28"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M15.5 11.75a3.5 3.5 0 11-7 0 3.5 3.5 0 017 0zm1.444-.75a5.001 5.001 0 00-9.888 0H2.75a.75.75 0 100 1.5h4.306a5.001 5.001 0 009.888 0h4.306a.75.75 0 100-1.5h-4.306z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              <a>
+                <h3 className="p-0 m-0 pt-2 text-4xl font-bold mb-4 dark:text-gray-200">
+                  Software Engineer Intern
+                </h3>
+              </a>
+              <p className="p-0 m-0 text-3xl mb-2 dark:text-gray-200">
+                Model N
+              </p>
+              <p className="p-0 m-0 text-2xl dark:text-gray-200 text-gray-500 before:content-['['] dark:before:text-teal-500 before:text-indigo-500 after:text-indigo-500 after:font-bold before:font-bold after:content-[']'] dark:after:text-teal-500 after:text-indigo-500after:font-bold">
+                &nbsp; May 2020  - Dec 2020 &nbsp;
+              </p>
+            </div>
+          </div>
+          <p className="relative pb-4 md:border-b dark:text-gray-200 md:border-gray-300 md:dark:border-gray-400 md:col-span-3 text-[1.7rem] font-sans">
+          • Developed full-stack application using Spring Boot for REST API microservices, optimizing CRUD operations, and
+            integrating with a React-based frontend. <br />
+          • Enhanced user experience by implementing robust front-end and back-end pagination across all features.<br />
+          • Architected a flexible framework for webhooks using Lambda, enabling automatic email triggers and data synchronization.<br />
+          • Played a pivotal role in Apache Kafka integration, facilitating efficient data transfer across multiple organizations, which
+            improved API performance by about 20% and upgraded data handling capabilities.<br />
           </p>
         </Article>
       </section>
